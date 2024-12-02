18da9a69f2a97ee431f3b3c6f97107941c364495
SacBruine237275 <72806803+SacBruine237275@users.noreply.github.com>
2024-12-02 13:37:31 +0100

    Solution of Day 2 Part 1

--- Day_2/Day_2/Program.cs
+++ Day_2/Day_2/Program.cs
@@ -11,27 +11,48 @@ namespace Day_2
     {
         static void Main(string[] args)
         {
-            ReadFile();
+            Console.WriteLine(ReadFile());
         }
 
-        public static void ReadFile()
+        public static int ReadFile()
         {
             try
             {
-                Console.WriteLine(Directory.GetCurrentDirectory());
+                int safeCount = 0;
                 using (StreamReader reader=File.OpenText("input.txt"))
                 {
                     string line;
-                    while((line=reader.ReadLine())!=null)
+                    while ((line=reader.ReadLine())!=null)
                     {
                         int[] valueArray =Array.ConvertAll(line.Split(' '),int.Parse);
+                        int validDifferenceCount = 0;
+                        bool increase = true;
+                        bool decrease = true;
+                        for (int i = 0; i < valueArray.Length-1; i++)
+                        {
+                            int differenceBetwennValue=Math.Abs(valueArray[i]-valueArray[i+1]);
+                            if (differenceBetwennValue==0 || differenceBetwennValue> 3)
+                                break;
+
+                            if (valueArray[i] < valueArray[i + 1])
+                                decrease = false;
+                            if (valueArray[i] > valueArray[i + 1])
+                                increase = false;
+                            validDifferenceCount++;
+                        }
+                        if (validDifferenceCount == valueArray.Length-1 && (increase || decrease))
+                            safeCount++;
+
                     }
                 }
+                return safeCount;
             }
             catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
+                return 0;
             }
         }
+
     }
 }
