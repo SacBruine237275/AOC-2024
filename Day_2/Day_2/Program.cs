04a348330c144117133dab11b4160083babe8d38
SacBruine <gregorytacquet@gmail.com>
2024-12-02 11:09:21 +0100

    Start of Day 2 of the AOC

---
+++ Day_2/Day_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadFile();
+        }
+
+        public static void ReadFile()
+        {
+            try
+            {
+                Console.WriteLine(Directory.GetCurrentDirectory());
+                using (StreamReader reader=File.OpenText("input.txt"))
+                {
+                    string line;
+                    while((line=reader.ReadLine())!=null)
+                    {
+                        int[] valueArray =Array.ConvertAll(line.Split(' '),int.Parse);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
