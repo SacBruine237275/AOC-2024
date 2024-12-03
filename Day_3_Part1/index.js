8caf32af0429175afb4479af7e41054dbd695e32
SacBruine237275 <72806803+SacBruine237275@users.noreply.github.com>
2024-12-03 13:59:39 +0100

    Resolution of Part 1 of Day 3

---
+++ Day_3_Part1/index.js
@@ -0,0 +1,17 @@
+var fs=require('fs-extra')
+
+var data = fs.readFileSync('input.txt', 'utf8');
+var result=0;
+const regex = /mul\(\s*\d+\s*,\s*\d+\s*\)/g;
+while ((array = regex.exec(data)) !== null) {
+    // Expected output: "Found foo. Next starts at 9."
+    // Expected output: "Found foo. Next starts at 19."
+    const regexForHaveNumber = /mul\(\s*(\d+)\s*,\s*(\d+)\s*\)/g;
+    while(((match=regexForHaveNumber.exec(array[0])))!=null){
+        const number1=parseInt(match[1],10)
+        const number2=parseInt(match[2],10)
+        result+=number1*number2;
+    }
+  }
+
+  console.log(result)
