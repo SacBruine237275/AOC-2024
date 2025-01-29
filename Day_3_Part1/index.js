39e7f29e80b01b544d72494ed3542f5fc42ba406
SacBruine237275 <72806803+SacBruine237275@users.noreply.github.com>
2025-01-29 15:58:35 +0100

    Remove commented expected comment from Day 3 Part 1 code

--- Day_3_Part1/index.js
+++ Day_3_Part1/index.js
@@ -4,8 +4,6 @@ var data = fs.readFileSync('input.txt', 'utf8');
 var result=0;
 const regex = /mul\(\s*\d+\s*,\s*\d+\s*\)/g;
 while ((array = regex.exec(data)) !== null) {
-    // Expected output: "Found foo. Next starts at 9."
-    // Expected output: "Found foo. Next starts at 19."
     const regexForHaveNumber = /mul\(\s*(\d+)\s*,\s*(\d+)\s*\)/g;
     while(((match=regexForHaveNumber.exec(array[0])))!=null){
         const number1=parseInt(match[1],10)
